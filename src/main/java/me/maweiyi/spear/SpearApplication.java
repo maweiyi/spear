aab8a4ea4aed4175ed339a7f26a2d60697e4961d
maweiyi <ma823285807@gmail.com>
2017-09-29 21:05:33 +0800

    init

---
+++ src/main/java/me/maweiyi/spear/SpearApplication.java
@@ -0,0 +1,12 @@
+package me.maweiyi.spear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpearApplication.class, args);
+	}
+}

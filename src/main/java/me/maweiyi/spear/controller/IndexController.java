ef2243bce5ff0c9f674cb0df8c48f3e5b4845f23
maweiyi <ma823285807@gmail.com>
2017-09-29 21:09:55 +0800

    init

---
+++ src/main/java/me/maweiyi/spear/controller/IndexController.java
@@ -0,0 +1,13 @@
+package me.maweiyi.spear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}

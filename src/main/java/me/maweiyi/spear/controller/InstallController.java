26a793d47d715acb1464e799b0f721eb0bfeb137
maweiyi <ma823285807@gmail.com>
2017-10-08 08:46:49 +0800

    日志

--- src/main/java/me/maweiyi/spear/controller/InstallController.java
+++ src/main/java/me/maweiyi/spear/controller/InstallController.java
@@ -1,10 +1,16 @@
 package me.maweiyi.spear.controller;
 
+import me.maweiyi.spear.R;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
+@RequestMapping("install")
 public class InstallController extends BaseController {
 
     @RequestMapping("/")
@@ -13,6 +19,12 @@ public class InstallController extends BaseController {
         mv.addObject("is_install", true);
         mv.setViewName("install");
         return mv;
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public R doInstall(HttpServletRequest request) {
+        return R.ok();
     }
 
 }
